78413ea6ee0058494d4e7d1a1d700a6bdc71052e
Sergio Correa <felizjanuk12@gmail.com>
2023-04-05 14:57:57 -0500

    feat(home-page): add session validation, add landing component

---
+++ src/components/landing.tsx
@@ -0,0 +1,17 @@
+import { signIn } from "next-auth/react"
+import Image from "next/image"
+
+const Landing = () => {
+  return (
+    <header className="bg-background">
+      <nav className="flex container justify-between mx-auto py-5 px-5 xl:px-38">
+        <Image src="/black_logo.svg" alt="logo full size" width={220} height={220} />
+        <button className="bg-primary rounded-md w-20 text-white" onClick={() => signIn()}>
+          Sign in
+        </button>
+      </nav>
+    </header>
+  )
+}
+
+export default Landing
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { type NextPage } from "next";
 import { useState } from "react";
 import { api } from "@/utils/api";
+import { signOut, useSession } from "next-auth/react";
+import Landing from "@/components/landing";
 
 const Home: NextPage = () => {
   const [postContent, setPostContent] = useState('')
+  const session = useSession()
   const utils = api.useContext()
   const { data: posts } = api.post.getAll.useQuery()
   const { mutate: createPost } = api.post.create.useMutation({
@@ -11,15 +14,22 @@ const Home: NextPage = () => {
       utils.post.getAll.invalidate().catch((e) => console.error(e))
     },
   })
+  console.log({ session })
   return (
     <>
-      <input value={postContent} onChange={(e) => setPostContent(e.target.value)} />
-      <button onClick={() => createPost({ postContent })}>Post</button>
-      {posts && posts.map((post) => (
-        <div key={post.id}>
-          <h1>{post.text}</h1>
-        </div>
-      ))}
+      {session.status === "unauthenticated" && <Landing />}
+      {session.status === "authenticated" && (
+        <>
+          <input value={postContent} onChange={(e) => setPostContent(e.target.value)} />
+          <button onClick={() => createPost({ postContent })}>Post</button>
+          {posts && posts.map((post) => (
+            <div key={post.id}>
+              <h1>{post.text}</h1>
+            </div>
+          ))}
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
     </>
   );
 };
