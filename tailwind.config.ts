43b4cc35ecfad87ae50e7dc0b7d449126602c4da
Sergio Correa <felizjanuk12@gmail.com>
2023-04-05 14:57:24 -0500

    feat(tailwind): add brand colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff325d",
+        background: "#faf8ff",
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
