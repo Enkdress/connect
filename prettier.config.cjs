f930eeefa5ac8fea4321dca34fbfeeb01d0d9006
Sergio Correa <felizjanuk12@gmail.com>
2023-04-05 14:56:24 -0500

    feat(prettier): add an organize input plugin and add default settings

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,18 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
+	plugins: [
+		require.resolve('prettier-plugin-organize-imports'),
+		require.resolve('prettier-plugin-tailwindcss')
+	],
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: false,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: 'avoid'
+}
 
-module.exports = config;
+module.exports = config
