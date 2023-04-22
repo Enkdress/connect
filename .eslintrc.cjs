6216e12c4298b8425de5b4b091ff7aba0b431419
Sergio Correa <felizjanuk12@gmail.com>
2023-04-22 18:10:00 -0500

    fix(eslint): make console logs a warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,7 +21,7 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
-    "no-console": "error",
+    "no-console": "warn",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
