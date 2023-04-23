e5272b881c8184f56e827357724cbc94dee5e66d
Sergio Correa <felizjanuk12@gmail.com>
2023-04-23 16:10:00 -0500

    feat(app): make it responsive

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,25 +1,42 @@
-import { signIn } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 
-const Landing = () => {
+export const Header = () => {
+  const session = useSession()
+  const isAuthenticated = !!session.data?.user
+
   return (
     <header className="bg-black2">
       <nav className="xl:px-38 container mx-auto flex justify-between px-5 py-5">
         <Image
           src="/logo_full_size.svg"
           alt="logo full size"
-          width={220}
-          height={220}
+          width={160}
+          height={160}
         />
-        <button
-          className="w-20 rounded-md bg-primary text-black1"
-          onClick={() => void signIn()}
-        >
-          Sign in
-        </button>
+        {!isAuthenticated && (
+          <button
+            className="w-20 rounded-md bg-primary text-black1"
+            onClick={() => void signIn()}
+          >
+            Sign in
+          </button>
+        )}
+        {isAuthenticated && (
+          <button
+            className="w-20 rounded-md bg-primary text-black1"
+            onClick={() => void signOut()}
+          >
+            Sign out
+          </button>
+        )}
       </nav>
     </header>
   )
+}
+
+const Landing = () => {
+  return <Header />
 }
 
 export default Landing
